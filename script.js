321af3414d9e33d043131f5b8f9133b3ff58a613
MiniRaccoonDog <atorres@spoonflower.com>
2018-05-15 15:25:22 -0400

    site works. Has filler content atm, for styling. gets replaced on pageload. name isn't correct but its exactly what i have on github, so that's probably ok. Added some slight re-scaling/responsiveness

---
+++ script.js
@@ -0,0 +1,57 @@
+import request from 'superagent'
+
+function onLoad () {
+  console.log('Loaded!')
+  const query = 'miniraccoondog'
+  getData(query)
+}
+
+function getData (query) {
+  request.get(`https://api.github.com/users/${query}`)
+    .then(function (result) {
+      console.log('result', result.body)
+      createBio(result.body)
+    })
+}
+
+function createBio (body) {
+  var name = body.name
+  var url = body.html_url
+  var company = body.company
+  var website = body.blog
+  var githubname = body.login
+  // var website = body.html_url
+  var photo = body.avatar_url
+  var bio = body.bio
+
+  console.log(name, url, company, website)
+  var header = document.querySelector('.box1')
+  header.innerHTML = `<h1 class="name">${name}</h1>`
+  var body = document.querySelector('.box2')
+  body.innerHTML =
+  `<div class="bio">
+  <h2 class="bio__headings">The Basics</h2>
+  <div class="bio__info"><em>Name</em> ${name}</div>
+  <div class="bio__info"><em>Github Url</em> <a href="${url}">${githubname}</a></div>
+  <div class="bio__info"><em>Company</em> ${company}</div>
+  <div class="bio__info"><em>Website</em> <a href="${website}">${website}</a></div>
+  </div>
+  <div class="bio">
+    <h2 class="bio__headings">The Story</h2>
+    <div class="bio__intro">
+      ${bio}
+    </div>
+  </div>
+  <div class="bio__photo">
+  <img class="photo" src="${photo}">
+  </div>`
+}
+
+/* <section class="box1">
+<h1 class="name">Asdasd</h1>
+</section>
+<section class="box2">
+
+</section> */
+
+document.addEventListener('DOMContentLoaded', onLoad)
